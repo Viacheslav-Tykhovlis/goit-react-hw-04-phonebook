13e27ef1fdb2e66ed8794326c13d3ef3898e7d56
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2023-01-06 19:53:42 +0100

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
@@ -6,9 +6,10 @@ import { ContactList } from './ContactList/ContactList';
 import { Contact } from './Contact/Contact';
 
 export function App() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) || []
+  );
   const [filter, setFilter] = useState('');
-  const isFirstRender = useRef(true);
 
   const handleSubmit = contact => {
     if (isSaved(contact)) {
@@ -41,25 +42,6 @@ export function App() {
   };
 
   useEffect(() => {
-    const parseContacts = JSON.parse(window.localStorage.getItem('contacts'));
-
-    if (parseContacts) {
-      setContacts(parseContacts);
-    } else {
-      setContacts([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
